e2fcc50955a7dd5b351e68620de052ee07f651dd
Taku Nyabadza <67196996+takusimon@users.noreply.github.com>
2025-03-29 12:28:50 -0500

    files I was having issues with

---
+++ test/99_bottles.py
@@ -0,0 +1,5 @@
+for i in range(99,-1,-1):
+  print(f'{i} bottles of beer on the wall')
+  print(f'{i} bottles of beer')
+  print('Take one down, pass it around')
+  print(f'{i -1} bottles of beer on the wall')
---
+++ test/bug_catcher.py
@@ -0,0 +1,10 @@
+butterflies = 10
+beetles = 12
+ladybugs = 20
+total = butterflies + beetles + ladybugs
+
+print('I caught ' + str(butterflies) + ' 🦋 butterflies!')
+print('I caught ' + str(beetles) + ' 🪲 beetles!')
+print('I caught ' + str(ladybugs) +  '🐞 ladybugs!')
+
+print('I caught ' + str(total) + ' total bugs!')
